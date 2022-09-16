9777da6cf2c65afe174854d6a79bf185fdf23e38
begimaib <begimai.bolotbekova>
2022-09-16 20:50:23 +0800

    add, subtract, multiply, and divide functions are added

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from api_v1.views import add
 
 app_name = "api_v1"
 
-
 urlpatterns = [
     path("add/", add),
+    path("subtract/", add),
+    path("multiply/", add),
+    path("divide/", add),
 ]
